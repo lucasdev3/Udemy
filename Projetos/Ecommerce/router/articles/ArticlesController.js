433ef280b1efbeed80d2c85ae0c40c6729b7c690
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-05-25 01:25:49 -0300

    update

--- Projetos/Ecommerce/router/articles/ArticlesController.js
+++ Projetos/Ecommerce/router/articles/ArticlesController.js
@@ -2,7 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/admin/articles', (req, res) => {
+    res.render('admin/articles/index.ejs')
+})
 
+router.get('/admin/articles/new', (req, res) => {
+    res.render('admin/articles/new')
+})
+
+router.post('/articles/save', (req, res) => {
+    const { title } = req.body;
+    if (title) {
+
+        Article.create({
+            title: title,
+            slug: slugify(title)
+        })
+            .then(() => {
+                res.redirect('/admin/categories');
+            })
+
+    }else {
+        res.redirect('/admin/categories/new');
+    }
+})
 
 
 
--- Projetos/Ecommerce/router/categories/CategoriesController.js
+++ Projetos/Ecommerce/router/categories/CategoriesController.js
@@ -4,9 +4,6 @@ const slugify = require('slugify');
 
 const router = express.Router();
 
-router.get('/admin/categories/new', (req, res) => {
-    res.render('./admin/categories/new')
-})
 
 router.get('/admin/categories', (req, res) => {
 
@@ -16,6 +13,30 @@ router.get('/admin/categories', (req, res) => {
         })
 
     
+})
+
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new')
+})
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+    const { id } = req.params;
+
+    if(isNaN(id)) {
+        res.redirect('/admin/categories');
+    }
+
+    Category.findByPk(id)
+        .then((category) => {
+            if(category) {
+                res.render('admin/categories/edit', {category: category});
+            } else {
+                res.redirect('admin/categories');
+            }
+        }) 
+        .catch((error) => {
+            res.redirect('admin/categories');
+        })
 })
 
 router.post('/categories/save', (req, res) => {
@@ -56,6 +77,31 @@ router.post('/categories/delete', (req, res) => {
     } else {
         res.redirect('/admin/categories');
     }
+})
+
+router.post('/categories/update/:id', (req, res) => {
+
+    // const { id } = req.params;
+    const { id, title } = req.body;
+
+    console.log(id)
+
+    if(id) {
+        if(!isNaN(id)) {
+            Category.update({title: title, slug: slugify(title)}, 
+                {
+                    where: {id: id}
+                })
+                .then(() => {
+                    res.redirect('/admin/categories');
+                })
+        } else {
+            res.redirect('/admin/categories');
+        }
+    } else {
+        res.redirect('/admin/categories');
+    }
+
 })
 
 module.exports = router;
